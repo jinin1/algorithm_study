fc31491b0cf9d00df58bc1b7e8e6ec9aea74db30
jinin1 <117060164+jinin1@users.noreply.github.com>
2022-12-02 16:54:02 +0900

    Update 22_12_01_02.py

--- yejin/22_12_01_02.py
+++ yejin/22_12_01_02.py
@@ -1,8 +1,8 @@
 # 캐릭터의 좌표
+
+# 가로측 좌표 r과 세로측 좌표 u를 정하고 방향마다 움직임을 정해준다.
 def solution(keyinput, board):
-    r = 0
-    u = 0
-# 가로측 좌표 r과 세로측 좌표 u를 정한다.
+    r, u = 0, 0
     for i in keyinput:
         if i == "left":
             r -= 1
@@ -17,7 +17,7 @@ def solution(keyinput, board):
             r = board[0]//2
         elif r < board[0]//2*-1:
             r = board[0]//2*-1
-        if u > board[1]//2:
+        elif u > board[1]//2:
             u = board[1]//2
         elif u < board[1]//2*-1:
             u = board[1]//2*-1  
